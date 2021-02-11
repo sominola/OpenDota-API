e4bc7f0cfb57a0200623fa6cd37d1bbdaf4f7b56
Nikita Savchuk <nsavchuk224@gmail.com>
2021-02-11 22:44:56 +0200

    update

--- OpenDotaApi/OpenDota.cs
+++ OpenDotaApi/OpenDota.cs
@@ -1,15 +1,22 @@
+using OpenDotaApi.Api.PlayersByRank;
+
 namespace OpenDotaApi
 {
+    using Utilities;
     using Api.Player;
-
+    using Api.Match;
     public class OpenDota
     {
         public OpenDota()
         {
             var request = new RequestHandler();
             Player = new PlayerEndpoint(request);
+            Matches = new MatchesEndpoint(request);
+            PlayersByRank = new PlayersByRankEndpoint(request);
         }
 
+        public readonly IPlayersByRankEndpoint PlayersByRank;
+        public readonly IMatchesEndpoint Matches;
         public readonly IPlayerEndpoint Player;
     }
 }
