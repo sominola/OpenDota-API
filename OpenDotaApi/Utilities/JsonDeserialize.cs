d6d1f9dff8429d6609d437c58aeacb77f7731d7e
Nikita Savchuk <nsavchuk224@gmail.com>
2021-02-15 01:33:37 +0200

    add new Converters

--- OpenDotaApi/Utilities/JsonDeserialize.cs
+++ OpenDotaApi/Utilities/JsonDeserialize.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using OpenDotaApi.Utilities.JsonConverters;
 
 namespace OpenDotaApi.Utilities
@@ -10,6 +11,8 @@ namespace OpenDotaApi.Utilities
         {
             _options = new JsonSerializerOptions();
             _options.Converters.Add(new Int32Converter());
+            _options.Converters.Add(new DateTimeConverter());
+            _options.Converters.Add(new Int64Converter());
         }
         public T Deserialize<T>(string json)
         {
