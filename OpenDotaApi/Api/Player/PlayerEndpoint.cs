e368d02abe5d4a0f4b7de83b4a98b641dcd19ee8
Nikita Savchuk <nsavchuk224@gmail.com>
2021-02-15 01:28:32 +0200

    add new parameters

--- OpenDotaApi/Api/Player/PlayerEndpoint.cs
+++ OpenDotaApi/Api/Player/PlayerEndpoint.cs
@@ -30,34 +30,34 @@ namespace OpenDotaApi.Api.Player
             await _request.GetResponseAsync<Player>($"players/{playerId}");
 
         public async Task<WinLoss> GetWinLossAsync(long playerId, PlayerParameters parameters = null) =>
-            await _request.GetResponseAsync<WinLoss>($"players/{playerId}/wl" + parameters?.GetParameters());
+            await _request.GetResponseAsync<WinLoss>($"players/{playerId}/wl", parameters?.GetParameters());
 
         public async Task<List<RecentMatches>> GetRecentMatchesAsync(long playerId) =>
             await _request.GetResponseAsync<List<RecentMatches>>($"players/{playerId}/recentMatches");
 
         public async Task<List<Matches>> GetMatchesAsync(long playerId, PlayerParameters parameters = null) =>
-            await _request.GetResponseAsync<List<Matches>>($"players/{playerId}/matches" + parameters?.GetParameters());
+            await _request.GetResponseAsync<List<Matches>>($"players/{playerId}/matches", parameters?.GetParameters());
 
         public async Task<List<Model.Heroes.Heroes>> GetHeroesAsync(long playerId, PlayerParameters parameters = null) =>
-            await _request.GetResponseAsync<List<Model.Heroes.Heroes>>($"players/{playerId}/heroes" + parameters?.GetParameters());
+            await _request.GetResponseAsync<List<Model.Heroes.Heroes>>($"players/{playerId}/heroes", parameters?.GetParameters());
 
         public async Task<List<Peers>> GetPeersAsync(long playerId, PlayerParameters parameters = null) =>
-            await _request.GetResponseAsync<List<Peers>>($"players/{playerId}/peers" + parameters?.GetParameters());
+            await _request.GetResponseAsync<List<Peers>>($"players/{playerId}/peers", parameters?.GetParameters());
 
         public async Task<List<Pro>> GetProAsync(long playerId, PlayerParameters parameters = null) =>
-            await _request.GetResponseAsync<List<Pro>>($"players/{playerId}/pros" + parameters?.GetParameters());
+            await _request.GetResponseAsync<List<Pro>>($"players/{playerId}/pros", parameters?.GetParameters());
 
         public async Task<List<Totals>> GetTotalsAsync(long playerId, PlayerParameters parameters = null) =>
-            await _request.GetResponseAsync<List<Totals>>($"players/{playerId}/totals" + parameters?.GetParameters());
+            await _request.GetResponseAsync<List<Totals>>($"players/{playerId}/totals", parameters?.GetParameters());
 
         public async Task<List<Histograms>> GetHistogramsAsync(long playerId, string field, PlayerParameters parameters = null) =>
-            await _request.GetResponseAsync<List<Histograms>>($"players/{playerId}/histograms/{field}" + parameters?.GetParameters());
+            await _request.GetResponseAsync<List<Histograms>>($"players/{playerId}/histograms/{field}", parameters?.GetParameters());
 
         public async Task<List<WardMap>> GetWardMapAsync(long playerId, PlayerParameters parameters = null) =>
-            await _request.GetResponseAsync<List<WardMap>>($"players/{playerId}/wardmap" + parameters?.GetParameters());
+            await _request.GetResponseAsync<List<WardMap>>($"players/{playerId}/wardmap", parameters?.GetParameters());
 
         public async Task<List<WordCloud>> GetWordCloudAsync(long playerId, PlayerParameters parameters = null) =>
-            await _request.GetResponseAsync<List<WordCloud>>($"players/{playerId}/wordcloud" + parameters?.GetParameters());
+            await _request.GetResponseAsync<List<WordCloud>>($"players/{playerId}/wordcloud", parameters?.GetParameters());
 
         public async Task<List<Ratings>> GetRatingsAsync(long playerId) =>
             await _request.GetResponseAsync<List<Ratings>>($"players/{playerId}/ratings");
