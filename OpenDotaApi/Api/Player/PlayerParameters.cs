e0e46f6edae79b78f2b72b4696b158407d93e13e
Nikita Savchuk <nsavchuk224@gmail.com>
2021-03-07 02:54:54 +0200

    change namespace

--- OpenDotaApi/Api/Player/PlayerParameters.cs
+++ OpenDotaApi/Api/Player/PlayerParameters.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Text.RegularExpressions;
 
-namespace OpenDotaApi.Api.Player
+namespace OpenDotaApi
 {
     public class PlayerParameters
     {
