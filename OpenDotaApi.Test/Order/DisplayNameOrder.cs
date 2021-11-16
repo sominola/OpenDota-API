40dd3cf055f44f2c63d1712230a904ae84cb3398
sominola <nsavchuk224@gmail.com>
2021-11-16 18:20:05 +0200

    Add test order

---
+++ OpenDotaApi.Test/Order/DisplayNameOrder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OpenDotaApi.Test.Order
+{
+    public class DisplayNameOrderer : ITestCollectionOrderer
+    {
+        public IEnumerable<ITestCollection> OrderTestCollections(
+            IEnumerable<ITestCollection> testCollections) =>
+            testCollections.OrderBy(collection => collection.DisplayName);
+    }
+}
